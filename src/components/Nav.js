5395ca8b188796efa0ab807e328384c342c7e86b
Hien Nguyen <hiennguyen1529@gmail.com>
2020-10-20 22:22:30 +1100

    nav responsive with hamburger

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from "react"
 import NavLinks from "../constants/NavLinks"
+import { FaAlignRight } from "react-icons/fa"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
@@ -10,6 +11,44 @@ const NavStyles = styled.div`
   grid-template-columns: 1fr 1fr;
   align-items: center;
   color: var(--red);
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .btn-menu {
+    display: none;
+    color: var(--red);
+    font-size: 2.4rem;
+    background: transparent;
+    border: none;
+    outline: 0;
+    cursor: pointer;
+    @media (max-width: 900px) {
+      display: block;
+    }
+  }
+  .nav-links {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    li {
+      margin-left: 4rem;
+    }
+    a {
+      font-size: 1.8rem;
+      &:hover {
+        color: var(--red);
+      }
+    }
+    @media (max-width: 900px) {
+      display: none;
+    }
+  }
 `
 
 const query = graphql`
@@ -34,9 +73,14 @@ const Nav = () => {
     <div className="container">
       <div className="inner-container">
         <NavStyles>
-          <Link to="/">
-            <Img fixed={fixed} className="logo" />
-          </Link>
+          <div>
+            <Link to="/">
+              <Img fixed={fixed} className="logo" />
+            </Link>
+            <button type="button" className="btn-menu">
+              <FaAlignRight></FaAlignRight>
+            </button>
+          </div>
           <NavLinks />
         </NavStyles>
       </div>
--- src/constants/NavLinks.js
+++ src/constants/NavLinks.js
@@ -25,27 +25,9 @@ const data = [
   },
 ]
 
-const NavLinksStyles = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  li {
-    margin-left: 4rem;
-  }
-  a {
-    font-size: 1.8rem;
-    &:hover {
-      color: var(--red);
-    }
-    /* &[aria-current="page"] {
-      text-decoration: underline;
-    } */
-  }
-`
-
 const NavLinks = ({ styleClass }) => {
   return (
-    <NavLinksStyles className={`nav-links ${styleClass ? styleClass : ""}`}>
+    <ul className={`nav-links ${styleClass ? styleClass : ""}`}>
       {data.map(link => {
         return (
           <li key={link.id}>
@@ -53,7 +35,7 @@ const NavLinks = ({ styleClass }) => {
           </li>
         )
       })}
-    </NavLinksStyles>
+    </ul>
   )
 }
 
